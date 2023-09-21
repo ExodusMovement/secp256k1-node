b52975290dfab731796e2759f41a58a9c3b2a1ed
Vodopyanov Egor <egorvodopyanov@gmail.com>
2023-09-21 22:30:31 +0300

    move to elliptic

--- elliptic.js
+++ elliptic.js
@@ -1,1 +1,29 @@
-module.exports = require('./lib')(require('./lib/elliptic'))
+const defaultLib = require('./lib')(require('./lib/elliptic'))
+const secp = require('@bitcoinerlab/secp256k1')
+module.exports = {
+    ...defaultLib,
+    publicKeyCreate (seckey, compressed = true) {
+        return secp.pointFromScalar(seckey, compressed)
+    },
+    privateKeyVerify (seckey) {
+        return secp.isPrivate(seckey)
+    },
+    publicKeyVerify (seckey) {
+        return secp.isPoint(seckey)
+    },
+    publicKeyConvert (seckey, compressed) {
+        return secp.pointCompress(seckey, compressed)
+    },
+    privateKeyTweakAdd (seckey, tweak) {
+        return secp.privateAdd(seckey, tweak)
+    },
+    publicKeyTweakAdd (seckey, tweak, compress) {
+        return secp.pointAddScalar(seckey, tweak, compress)
+    },
+    ecdsaSign (hash, privateKey, options = {}) {
+        return secp.sign(hash, privateKey, options.data)
+    },
+    ecdsaVerify (sign, hash, publicKey) {
+        return secp.verify(hash, publicKey, sign)
+    },
+}
