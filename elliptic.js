bc6b3bd07b056cc0000d5261ad2de1e020023850
Alexander Pataridze <s.pataridze@gmail.com>
2024-01-10 20:11:40 +0400

    refactor: cover unimplemented apis

--- elliptic.js
+++ elliptic.js
@@ -6,16 +6,41 @@ if (!BigIntSupportChecked)
     BigInt(1);
     const noble_secp256k1 = require("@exodus/bitcoinerlab-secp256k1");
     secp256k1 = {
-      publicKeyCreate: function (privateKey, compressed, output) {
-        output = noble_secp256k1.pointFromScalar(privateKey, compressed);
-        return output;
+      contextRandomize: function () {
+        return 0;
+      },
+      privateKeyVerify: function (privateKey) {
+        return noble_secp256k1.isPrivate(privateKey);
+      },
+      privateKeyNegate: function (privateKey) {
+        return noble_secp256k1.privateNegate(seckey);
+      },
+      privateKeyTweakAdd: function (privateKey, tweak) {
+        return noble_secp256k1.privateAdd(privateKey, tweak);
+      },
+      privateKeyTweakMul: function (privateKey, tweak) {
+        throw new Error("privateKeyTweakMul is not implemented");
       },
       publicKeyVerify: function (publicKey) {
         return noble_secp256k1.isPoint(publicKey);
       },
+      publicKeyCreate: function (privateKey, compressed, output) {
+        if (output) {
+          throw new Error("publicKeyCreate | output param not handled");
+        }
+        return noble_secp256k1.pointFromScalar(privateKey, compressed);
+      },
       publicKeyConvert: function (publicKey, compressed, output) {
-        output = noble_secp256k1.isPoint(publicKey, compressed);
-        return output;
+        if (output) {
+          throw new Error("publicKeyConvert | output param not handled");
+        }
+        return noble_secp256k1.pointCompress(publicKey, compressed);
+      },
+      publicKeyNegate: function (publicKey, compressed, output) {
+        throw new Error("publicKeyNegate is not implemented");
+      },
+      publicKeyCombine: function (publicKeys, compressed, output) {
+        throw new Error("publicKeyCombine is not implemented");
       },
       publicKeyTweakAdd: function (
         publicKey,
@@ -25,20 +50,42 @@ if (!BigIntSupportChecked)
       ) {
         return noble_secp256k1.pointAddScalar(publicKey, tweak, compressed);
       },
-      privateKeyNegate: function (seckey) {
-        return noble_secp256k1.privateNegate(seckey);
+      publicKeyTweakMul: function (publicKey, tweak, compressed, ouput) {
+        if (output) {
+          throw new Error("publicKeyTweakMul | output param not handled");
+        }
+        return noble_secp256k1.pointMultiply(publicKey, tweak, compressed);
       },
-      privateKeyVerify: function (privateKey) {
-        return noble_secp256k1.isPrivate(privateKey);
+      signatureNormalize: function (signature) {
+        throw new Error("signatureNormalize is not implemented");
       },
-      privateKeyTweakAdd: function (privateKey, tweak) {
-        return noble_secp256k1.privateAdd(privateKey, tweak);
+      signatureExport: function (signature, output) {
+        throw new Error("signatureExport is not implemented");
       },
-      ecdsaVerify: function (sig, msg32, pubkey) {
-        return noble_secp256k1.verify(msg32, pubkey, sig);
+      signatureImport: function (signature, output) {
+        throw new Error("signatureImport is not implemented");
       },
-      ecdsaSign: function (msg32, seckey, options = {}, output) {
-        return { signature: secp256k1.sign(msg32, seckey) };
+      ecdsaSign: function (message, privateKey, options = {}, output) {
+        if (output) {
+          throw new Error("ecdsaSign | output param not handled");
+        }
+        return {
+          signature: noble_secp256k1.sign(message, privateKey),
+          get recid() {
+            throw new Error(
+              "ecdsaSign | recoverable signing is not implemented"
+            );
+          },
+        };
+      },
+      ecdsaVerify: function (signature, message, publicKey) {
+        return noble_secp256k1.verify(message, publicKey, signature);
+      },
+      ecdsaRecover: function (signature, recid, compressed, output) {
+        throw new Error("ecdsaRecover is not implemented");
+      },
+      ecdh: function (publicKey, privateKey, opts) {
+        throw new Error("ecdh is not implemented");
       },
     };
   } catch (e) {
