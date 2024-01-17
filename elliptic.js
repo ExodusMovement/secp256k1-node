d28dd45ed3744599bf16c4fbdcbbec39a836862f
Alexander Pataridze <s.pataridze@gmail.com>
2024-01-17 14:48:37 +0400

    refactor: donot hijack elliptic.js

--- elliptic.js
+++ elliptic.js
@@ -1,15 +1,2 @@
-let BigIntSupportChecked = false
-let secp256k1
-
-if (!BigIntSupportChecked) {
-  try {
-    BigInt(1)
-    secp256k1 = require('./lib')(require('./lib/bitcoinerlab'), true)
-  } catch (e) {
-    secp256k1 = require('./lib')(require('./lib/elliptic'))
-  } finally {
-    BigIntSupportChecked = true
-  }
-}
-
-module.exports = secp256k1
+'use strict'
+module.exports = require('./lib')(require('./lib/elliptic'))
