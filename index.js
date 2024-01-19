e4cdce31c54274b1ed5381776e959aee7cc6689e
Alexander Pataridze <s.pataridze@gmail.com>
2024-01-19 15:20:25 +0400

    fix: package

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-let BigIntSupportChecked = false
 let secp256k1
 
-if (!BigIntSupportChecked) {
-  try {
-    BigInt(1)
-    secp256k1 = require('./bitcoinerlab')
-  } catch (e) {
-    secp256k1 = require('./elliptic')
-  } finally {
-    BigIntSupportChecked = true
-  }
+try {
+  BigInt(1)
+  secp256k1 = require('./bitcoinerlab')
+} catch (e) {
+  secp256k1 = require('./elliptic')
+} finally {
+  BigIntSupportChecked = true
 }
 
 module.exports = secp256k1
